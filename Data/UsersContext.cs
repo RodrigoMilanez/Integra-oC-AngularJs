673eed2c9866d9c3c1910c761ed29138ba720db2
Rodrigo Milanez <rodrigo.ohland@hotmail.com>
2022-09-25 17:00:31 -0300

    inicio

---
+++ Data/UsersContext.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+        {
+
+        }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication2.Models.Users>? Users { get; set; }
+
+    }
+
+
+}
---
+++ Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Models
+{
+    public class Users
+    {
+        public Users()
+        {
+
+        }
+        
+        public int id { get; set; }
+        public string name { get; set; }
+        public string senha { get; set; }
+    }
+}
